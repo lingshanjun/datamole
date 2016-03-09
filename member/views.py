6209c88db2b00b3eeb76ce903a0f4e968a34d5bd
lwj <1198275198@qq.com>
2016-03-09 15:22:49 +0800

    member list FE

--- member/views.py
+++ member/views.py
@@ -6,10 +6,7 @@ from django.http import HttpResponseRedirect, HttpResponse,HttpResponseServerErr
 def memberList(request):
 	"""member首页"""
 	data ={}
-	# data['blog'] = Blog.objects.exclude(status=0).order_by('-id')	#blog列表
-	# data['bigtype'] = BlogBigType.objects.all().order_by('id')	#blog大分类
-	# data['hot'] = Blog.objects.exclude(status=0).order_by('-counts')[:10]	#blog 热门文章
-	# data['recomment'] = Blog.objects.exclude(status=0).filter(is_recomment=True).order_by('-id')[:10]	#blog 推荐文章
+	data['members'] = Member.objects.order_by('id')
 	return render(request, 'member_list.html', data)
 
 
