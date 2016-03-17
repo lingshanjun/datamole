1044c24e46efac6bc2e589f7682c52d486720676
lwj <1198275198@qq.com>
2016-03-17 14:03:09 +0800

    mono view chapter

--- monograph/views.py
+++ monograph/views.py
@@ -19,18 +19,22 @@ def monographDetail(request, id=None):
 
     if chapters:
         for chapter in chapters:
-            chaper = {}
+            ch = {}
             sections = MonoSection.objects.filter(chapter=chapter.id).order_by('order')
-            chaper['title'] = chapter.title
-            chaper['order'] = chapter.order
-            chaper['sections'] = sections
+            ch['title'] = chapter.title
+            ch['order'] = chapter.order
+            ch['sections'] = sections
 
-            data['chapters'].append(chaper)
+            data['chapters'].append(ch)
+
+    if 'corder' in request.GET:
+        corder = request.GET['corder']
+        chapter_detail = MonoChapter.objects.get(order=int(corder))
+        data['chapter_detail'] = chapter_detail
 
     if 'sorder' in request.GET:
         sorder = request.GET['sorder']
-        section = MonoSection.objects.get(order=float(sorder))
-        if section:
-            data['section'] = section
+        section_detail = MonoSection.objects.get(order=float(sorder))
+        data['section_detail'] = section_detail
 
     return render(request, 'monograph_detail.html', data)
