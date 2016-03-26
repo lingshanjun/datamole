7de8e10384cb51582af883b37ff21a327e4a4404
lwj <1198275198@qq.com>
2016-03-26 17:23:54 +0800

    fe add banner

--- datamole/views.py
+++ datamole/views.py
@@ -1,9 +1,11 @@
 # coding=utf-8
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseServerError
-
+from news.models import Slide
 
 def datamoleIndex(request):
     """datamole首页"""
     data = {}
+    slides = Slide.objects.order_by('priority').all()
+    data['slides'] = slides
     return render(request, 'datamole_index.html', data)
