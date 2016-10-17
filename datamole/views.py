8118c5b2778554b002bf6aa47b12f731739945ad
lwj <1198275198@qq.com>
2016-10-17 15:35:24 +0800

    recruit fe

--- datamole/views.py
+++ datamole/views.py
@@ -1,14 +1,14 @@
 # coding=utf-8
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseServerError
-from news.models import Slide
+from others.models import Recruit
 
 
 def datamoleIndex(request):
     """datamole首页"""
     data = {}
-    slides = Slide.objects.order_by('priority').all()
-    data['slides'] = slides
+    recruit = Recruit.objects.filter(is_show=True).order_by('-add_date').all()[0]
+    data['recruit'] = recruit
     return render(request, 'datamole_index.html', data)
 
 
--- others/models.py
+++ others/models.py
@@ -6,7 +6,7 @@ from django.utils.html import format_html
 
 class Recruit(models.Model):
     title = models.CharField('标题', max_length=100, help_text='*必填*,不在主页显示,用于标记各条招贤令内容')
-    recruit_content = models.TextField('招聘内容', help_text='*必填*')
+    recruit_content = models.TextField('招聘内容', help_text='*必填*,字体大小建议使用"大号字体",显示效果最佳,其他字体已此为基准自由选择.')
     is_show = models.BooleanField('首页显示', default=False, help_text="首页只能显示一条招贤令内容,当且仅当该项选中时,该条招贤令才可在首页正确显示.")
     add_date = models.DateTimeField('创建日期', auto_now_add=True)
 
